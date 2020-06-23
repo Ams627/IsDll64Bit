a5033f390b9cc7c6ef9eaf923d9a912f271b5338
Adrian Sims <adriansims62781@gmail.com>
2020-06-23 15:26:08 +0200

    Add project files.

---
+++ IsDll64Bit/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace IsDll64Bit
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (!args.Any())
+                {
+                    throw new Exception($"You must supply one or more filenames.");
+                }
+                foreach (var filename in args)
+                {
+                    var buffer = new byte[2048];
+                    using (var stream = new FileStream(filename, FileMode.Open, FileAccess.Read))
+                    {
+                        stream.Read(buffer, 0, 2048);
+                        var offset = BitConverter.ToInt32(buffer, 0x3c);
+                        var machineType = BitConverter.ToUInt16(buffer, offset + 4);
+                        Console.WriteLine($"0x{machineType:X}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine($"{progname} Error: {ex.Message}");
+            }
+
+        }
+    }
+}
